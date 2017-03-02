e321c7e5e3ddd3f4abc6cd3bdd079b3118f18f1a
Jack Phoebus <jmp496@drexel.edu>
2017-03-02 15:23:12 -0500

    Moved EnemySpawnerEditor to the newly created Editor folder so it wont cause errors on build updated the changelist unity possibly made changes to a few msc files so dunno if that matters or not but if it does blame this commit

---
+++ Assets/Editor/EnemySpawnerEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemySpawner))]
+public class EnemySpawnerEditor : Editor {
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+		var controller = target as EnemySpawner;
+		EditorGUIUtility.LookLikeInspector();
+
+		/*
+		SerializedProperty tps = serializedObject.FindProperty ("enemyTypes");
+		EditorGUI.BeginChangeCheck();
+		EditorGUILayout.PropertyField(tps, true);
+
+		tps = serializedObject.FindProperty ("spawnRegions");
+		EditorGUILayout.PropertyField(tps, true);//*/
+		EditorGUI.BeginChangeCheck();
+		SerializedProperty tps=serializedObject.GetIterator();
+		tps.NextVisible(true);
+		do{
+			EditorGUILayout.PropertyField(tps, true);
+			//tps=tps.Next(true);
+		}while(tps.NextVisible(false));
+
+		if(EditorGUI.EndChangeCheck()) {
+			serializedObject.ApplyModifiedProperties();
+		}
+		EditorGUIUtility.LookLikeControls();
+	}
+}
+
