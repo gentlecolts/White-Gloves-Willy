5396c5dfe8e18f6a0639081d3bb34f06640fa6b5
Phoebus <jmp496@drexel.edu>
2017-03-01 22:39:47 -0500

    Made wand movement better created health management prefab, does nothing right now

--- Assets/scripts/Player/WandScript.cs
+++ Assets/scripts/Player/WandScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WandScript : MonoBehaviour {
 	public float throwSpeed,throwCooldown;
 	public float wandRestRadius=16,wandSightRadius=100;
+	public float lockonSpeed=100;
 	
 	private GameObject player,wandSprite;
 	private bool isThrown=false;
@@ -32,6 +33,7 @@ public class WandScript : MonoBehaviour {
 
 			RaycastHit2D hit;
 			float hitD;
+			Vector3 target;
 			/*this particular chunk of code deserves some explaination, so here goes
 			 * first, we do a raycast from the player's position in the aim direction (but only as far as wandSightRadius and only for objects on the Enemy layer)
 			 * then, if there's a hit, we do a bit of math
@@ -44,10 +46,11 @@ public class WandScript : MonoBehaviour {
 			 *	simply set the wand to the rest radius
 			 */
 			if((hit=Physics2D.Raycast(player.transform.position,direction,wandSightRadius,enemyMask)) && (hitD=hit.distance/player.transform.localScale.x)>2*wandRestRadius){
-				wandSprite.transform.localPosition=new Vector3(0,hitD-wandRestRadius,0);
+				target=new Vector3(0,hitD-wandRestRadius,0);
 			}else {
-				wandSprite.transform.localPosition=new Vector3(0,wandRestRadius,0);
+				target=new Vector3(0,wandRestRadius,0);
 			}
+			wandSprite.transform.localPosition=Vector3.MoveTowards(wandSprite.transform.localPosition,target,lockonSpeed*Time.deltaTime);
 		}
 	}
 
