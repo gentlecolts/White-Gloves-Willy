42113dd6931980d7aacc76ad1a1a17003e1b71e4
James Colley <semaj234@gmail.com>
2017-03-02 18:04:40 -0500

    i changed barrel collision again
    i changed around the barrel configuration and ive changed the moon prop
    to despawn after it has been hit off the ropes. (but it only despwans
    the moon portion not the ropes)

---
+++ Assets/scripts/Projectiles/bananaMoon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bananaMoon : MonoBehaviour {
+
+    public float despawntime;
+    private float kill;
+
+    void Awake()
+    {
+        kill = despawntime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool need = gameObject.GetComponent<Rigidbody2D>().IsAwake();
+        if (need)
+        {
+            kill -= Time.deltaTime;
+            if (kill <= .1)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+		
+	}
+}
