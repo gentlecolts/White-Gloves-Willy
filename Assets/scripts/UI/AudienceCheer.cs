debc8a0b9174d24387f103c18d869bac501fc759
lily-lauben <lilydragondrawer@gmail.com>
2017-03-09 12:23:41 -0500

    Audio System and Pause Menu Framework
    Added Pause menu prefab, and audio components - still need to be hooked
    up to scripts.

---
+++ Assets/scripts/UI/AudienceCheer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceCheer : MonoBehaviour {
+
+	public AudioSource Audience_Mad;
+	public AudioSource Audience_Ok;
+	public AudioSource Audience_Happy;
+
+	private float happyLvl;
+
+	public void UpdateHappiness(float happinessLvl) {
+		happyLvl = happinessLvl;
+	}
+
+	public void AudienceNoise() {
+		if (happyLvl < 0.33f) {
+			Audience_Mad.Play ();
+		} else if (happyLvl > 0.66f) {
+			Audience_Happy.Play ();
+		} else {
+			Audience_Ok.Play ();
+		}
+	}
+}
---
+++ Assets/scripts/UI/VolumeChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class VolumeChange : MonoBehaviour {
+
+	public AudioMixer mixer;
+
+	public void SetMasterVol (float masterVolLvl) {
+		mixer.SetFloat ("MasterVol", masterVolLvl);
+	}
+
+	public void SetMusicVol (float musicVolLvl) {
+		mixer.SetFloat ("MusicVol", musicVolLvl);
+	}
+
+	public void SetSFXVol (float SFXVolLvl) {
+		mixer.SetFloat ("SFXVol", SFXVolLvl);
+	}
+
+	public void reset(Slider slider){
+		slider.value = 0;
+	}
+}
