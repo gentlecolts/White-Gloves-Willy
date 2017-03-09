46a33ef06399b8fec17b8e20080de87b69c037f5
lily-lauben <lilydragondrawer@gmail.com>
2017-03-08 23:21:10 -0500

    Created Start of a Tutorial Scene
    Still needs a lot of work, but need to do other things.

---
+++ Assets/scripts/UI/TutorialController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+	public Text textTut;
+
+	private string[] tutWords;
+	private int currentText;
+
+	// Use this for initialization
+	void Start () {
+		currentText = 0;
+		tutWords = new string[10];
+
+		tutWords [0] = "Help Willie the Magician save his disastrous magic show!" +
+			" Use WASD/Arrow Keys to move around the stage. Space is jump.";
+		tutWords [1] = "As a magician, you have abilities. Use Left Mouse to sent out a" +
+		" sparkle that will swap positions between you and another object.";
+		textTut.text = tutWords [currentText];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void nextIsClicked(){
+		currentText += 1;
+		textTut.text = tutWords [currentText];
+	}
+
+}
