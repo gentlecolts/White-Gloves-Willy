40846d495278110267cd0ce21aead4536490a8cb
lily-lauben <lilydragondrawer@gmail.com>
2017-03-08 23:41:46 -0500

    Added AudienceMeter Script
    Allows alteration of the audience bar fill through the AudienceMeter
    Script. Woo.

---
+++ Assets/scripts/UI/AudienceMeter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudienceMeter : MonoBehaviour {
+
+	public float HappyLevel;
+	public Image audiencefill;
+
+	void Start () {
+	}
+	
+	void Update () {
+		audiencefill.fillAmount = HappyLevel;
+		//HappyLevel should be a float between 0 and 1, 0 is empty and 1 is full.
+		//It also probably won't be public in the final script.
+		//Yeah.
+	}
+}
