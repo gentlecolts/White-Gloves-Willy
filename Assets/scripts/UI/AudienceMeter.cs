c9d50adf544728f060a667e98a823df0244e72f9
lily-lauben <lilydragondrawer@gmail.com>
2017-03-14 23:24:04 -0400

    Fixed Player and Audience Noise
    Player and Audience noise is now prefab-based and working.

--- Assets/scripts/UI/AudienceMeter.cs
+++ Assets/scripts/UI/AudienceMeter.cs
@@ -17,11 +17,15 @@ public class AudienceMeter : MonoBehaviour {
 	}
 
 	public Image audiencefill;
+	public float delayUntilCheer;
+	public AudioSource bigCheerNoise;
+	public AudioSource smallCheerNoise;
+	public AudioSource booNoise;
 
 	[Range(0,happyMax)]
-	public float startValue,smallCheerAmount,bigCheerAmount,drainPerSec;
+	public float startValue,unhappyLevel,smallCheerAmount,bigCheerAmount,drainPerSec;
 	
-
+	private bool isHappy=true, booing=false;
 
 	void Start () {
 		instance=this;
@@ -34,15 +38,43 @@ public class AudienceMeter : MonoBehaviour {
 
 		happyLevel=Mathf.Clamp(happyLevel,0,happyMax);
 		audiencefill.fillAmount = happyLevel/happyMax;
+
+		if (HappyLevel < unhappyLevel) {
+			isHappy = false;
+			if (!booing) {
+				StartCoroutine (NotHappy ());
+				booing = true;
+			}
+		}else {
+			isHappy= true;
+		}
 	}
 
 	//possibly have audience react to these
 	public void smallCheer() {
 		Debug.Log("small cheer!");
+		if (isHappy) {
+			StartCoroutine (MakeNoise (smallCheerNoise));
+		}
 		happyLevel+=smallCheerAmount;
 	}
 	public void bigCheer() {
 		Debug.Log("big cheer!");
+		if (isHappy) {
+			StartCoroutine (MakeNoise (bigCheerNoise));
+		}
 		happyLevel+=bigCheerAmount;
+	}
+
+	IEnumerator MakeNoise(AudioSource noise) {
+		yield return new WaitForSeconds (delayUntilCheer);
+		noise.Play ();
+	}
+
+	IEnumerator NotHappy() {
+		while (!isHappy) {
+			booNoise.Play ();
+			yield return new WaitForSeconds (8f);
+		}
 	}
 }
