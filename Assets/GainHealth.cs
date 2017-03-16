75d38593e15266cf0cd9cd609c1fad24e54a4a7b
lily-lauben <lilydragondrawer@gmail.com>
2017-03-16 01:26:09 -0400

    Throwable Heart Part 3
    THE HEART RETURNS!

--- Assets/GainHealth.cs
+++ Assets/GainHealth.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class GainHealth : MonoBehaviour {
 
-	public PlayerHealthManager health;
-
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "Player") {
-			health.health += 1;
+			PlayerHealthManager.Instance.health++;
 		}
+
 	}
 }
