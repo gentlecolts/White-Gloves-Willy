18864e9031b92a81ae896d1199d86132c820ee00
lily-lauben <lilydragondrawer@gmail.com>
2017-03-15 23:12:05 -0400

    TutorialChanges Part1
    First half of tutorial changes. Got all text working. More to come.

--- Assets/scripts/UI/TutorialController.cs
+++ Assets/scripts/UI/TutorialController.cs
@@ -10,6 +10,8 @@ public class TutorialController : MonoBehaviour {
 	public GameObject Barrel;
 	public GameObject Fedora;
 	public GameObject RightBarrelSpawner;
+	public GameObject LeftBarrelSpawner;
+	public GameObject EnemySpawner;
 
 	private string[] tutWords;
 	private int currentText;
@@ -23,13 +25,26 @@ public class TutorialController : MonoBehaviour {
 			" Use WASD/Arrow Keys to move around the stage. Space is jump. Press Return to continue.";
 		tutWords [1] = "As a magician, you have abilities. Use Left Mouse to sent out a" +
 		" sparkle that will swap positions between you and another object.";
-		tutWords[2] = "Hats are your enemies! Keep them far away from you with your teleport, and " +
+		tutWords [2] = "Hats are your enemies! Keep them far away from you with your teleport, and " +
 			"your dash, which you can use with Right Mouse Button.";
 		tutWords [3] = "You may have noticed that if you dash or teleport into enemies, you can stun them, " +
 		"and they stop moving for a time.";
 		tutWords [4] = "Willie doesn't want to get his white gloves dirty, so he must use other means of " +
 		"getting rid of the hats permanently. Use barrels or other hats to destroy them.";
+		tutWords [5] = "I think you're beginning to get the hang of this! Now, hats come in multiple " +
+		"varieties, so watch out. Each poses a different threat.";
+		tutWords [6] = "There are also different kinds of props. Moons will kill enemies on contact, " +
+		"so they're very handy in a pinch.";
+		tutWords [7] = "Finally, there's the audience. This is still a show, so keeping the audience entertained is your " +
+		"primary objective! Other than survival, of course. The bar at the bottom displays audience happiness.";
+		tutWords [8] = "Audiences like excitement! Killing an enemy will increase their happiness. But audiences easily" +
+		" get bored, so their happiness steadily drains over time. Don't let them get too unhappy, or else it will be" +
+		"The End.";
+		tutWords [9] = "That's all, folks. Hitting Return one more time will take you back to the main menu.";
 		textTut.text = tutWords [currentText];
+
+		AudienceMeter.Instance.drainPerSec=0;
+
 	}
 	
 	// Update is called once per frame
@@ -47,10 +62,10 @@ public class TutorialController : MonoBehaviour {
 	void spawnAppropriateItems(){
 		switch (currentText) {
 		case 3:
+		case 4:
 		case 1:
 			Instantiate (Barrel, stageLeft, transform.rotation);
 			break;
-		case 4:
 		case 2: 
 			Instantiate (Fedora, stageRight, transform.rotation);
 			break;
