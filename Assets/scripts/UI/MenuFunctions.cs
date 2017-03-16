dcd36d0a1da53b95849385975bf179f3647990dd
lily-lauben <lilydragondrawer@gmail.com>
2017-03-15 21:45:52 -0400

    Separated out Intro Scene, changed Scene settings
    Yay now the intro scene will only run once, and will no longer be in
    the way!

--- Assets/scripts/UI/MenuFunctions.cs
+++ Assets/scripts/UI/MenuFunctions.cs
@@ -6,24 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuFunctions : MonoBehaviour {
 
-	public GameObject StartImage;
-	public GameObject Menu;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
-
-	public void StartImageClick() {
-		Destroy (StartImage); //Fade out Coroutine later
-		Menu.gameObject.SetActive(true);
-	}
-
-
-	public void LoadByIndex(int sceneIndex)
+	public void LoadByIndex(string newScene)
 	{
-		SceneManager.LoadScene (sceneIndex);
+		SceneManager.LoadScene (newScene);
 	}
 
 
