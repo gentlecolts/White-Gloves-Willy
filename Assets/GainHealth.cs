4eb1dfc53cf3c9763f26e8c125cafda1110833db
lily-lauben <lilydragondrawer@gmail.com>
2017-03-16 01:27:29 -0400

    Throwable Heart Part 4
    THE TRAGIC DEATH OF THE HEART

--- Assets/GainHealth.cs
+++ Assets/GainHealth.cs
@@ -7,6 +7,7 @@ public class GainHealth : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "Player") {
 			PlayerHealthManager.Instance.health++;
+			Destroy (gameObject);
 		}
 
 	}
