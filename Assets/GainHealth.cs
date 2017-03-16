fb046766029b8b3b00d60337aee24d97a71447da
lily-lauben <lilydragondrawer@gmail.com>
2017-03-16 01:22:34 -0400

    Throwable Heart Part1
    Not quite working

---
+++ Assets/GainHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GainHealth : MonoBehaviour {
+
+	public PlayerHealthManager health;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag == "Player") {
+			health.health += 1;
+		}
+	}
+}
