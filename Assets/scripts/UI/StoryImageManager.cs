1a7020ec95c0f9f8c316326b18c2a56fff7e1697
lily-lauben <lilydragondrawer@gmail.com>
2017-03-15 21:21:22 -0400

    Created happy little intro story scenes
    yay

---
+++ Assets/scripts/UI/StoryImageManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryImageManager : MonoBehaviour {
+
+	private Image image;
+	public Text text;
+
+	void Start() {
+		image = GetComponent<Image> ();
+	}
+
+	public void OnClick() {
+		StartCoroutine (Fade ());
+		Destroy (gameObject, 1f);
+	}
+
+	IEnumerator Fade() {
+		for (float f = 1f; f >= 0; f -= 0.1f) {
+			Color c = image.color;
+			Color t = text.color;
+			c.a = f;
+			t.a = f;
+			image.color = c;
+			text.color = t;
+			yield return new WaitForSeconds(0.05f);
+		}
+	}
+
+}
