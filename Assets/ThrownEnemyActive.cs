c0185cab187b738c8842569d5ff58983ebea221d
lily-lauben <lilydragondrawer@gmail.com>
2017-03-23 01:09:07 -0400

    Created Script to trigger thrown Enemy
    @gentlecolts doesn’t quite work yet, but I believe it gets most of the
    way there?

---
+++ Assets/ThrownEnemyActive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownEnemyActive : MonoBehaviour {
+
+	public Collider2D col;
+
+	public void FreezeEnemies(){
+		col.enabled = false;
+	}
+
+	public void UnfreezeEnemies(){
+		col.enabled = true;
+	}
+
+}
